5ba322bbf4fdc710c9f52cc09c58d78a4477d2b1
srafi1 <shakilrafi0@gmail.com>
2019-06-11 14:03:03 -0400

    added css grid for campus card layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import CampusCard from './CampusCard.jsx';
 import './App.css';
 import './CampusCard.css';
@@ -7,9 +6,9 @@ import './CampusCard.css';
 function App() {
   return (
     <div className = "campuses-container">
-      <CampusCard name="Hunter" id="0" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents="1000" />
-      <CampusCard name="Hunter1" id="1" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents="1000" />
-      <CampusCard name="Hunter2" id="0" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents="1000" />
+      <CampusCard name="Hunter" id="0" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents={1000} />
+      <CampusCard name="Hunter1" id="1" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents={1000} />
+      <CampusCard name="Hunter2" id="0" image="https://www.voicesofyouth.org/sites/default/files/images/2019-03/school.jpg" numStudents={1000} />
     </div>
 
   );
--- src/CampusCard.jsx
+++ src/CampusCard.jsx
@@ -3,17 +3,17 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 //import './CampusCard.css';
 
-class CampusCard extends Component{
+class CampusCard extends Component {
 
   render(){
     return (
       <div className = "campus-card">
-        <img className = "campus-card-img"src={this.props.image}/>
+        <img className = "campus-card-img"src={this.props.image} alt={this.props.name + "image"} />
         <a className = "campus-card-title" href = {`/campuses/${this.props.id}`}> {this.props.name}</a>
         <p className = "campus-card-num"> {this.props.numStudents} Students </p>
         <div className = "campus-card-bot">
-          <a className = "campus-card-edit" href = {`/edit/campuses/${this.props.id}`}> edit </a>
-          <a className= "delete-campus-card" href= {`/delete/campuses/${this.props.id}`}> delete </a>
+          <a className = "campus-card-edit" href = {`/edit/campuses/${this.props.id}`}> edit</a> | 
+          <a className= "delete-campus-card" href= {`/delete/campuses/${this.props.id}`}> delete</a>
         </div>
       </div>
 
