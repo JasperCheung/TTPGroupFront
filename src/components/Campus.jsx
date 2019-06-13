4cc4790e90beb2f4a24bd7a107ba1574826232e1
srafi1 <shakilrafi0@gmail.com>
2019-06-13 14:17:56 -0400

    get campus info with thunk

--- src/components/Campus.jsx
+++ src/components/Campus.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchCampusThunk } from '../thunks';
 
 class Campus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campusId: props.match.params.id
-    };
+  componentDidMount = () => {
+    this.props.fetchCampus(this.props.match.params.id);
   }
 
   render = () => {
     return (
       <div>
-        <h1>Campus { this.state.campusId }</h1>
+        <h1>Campus { this.props.campus.id }</h1>
+        { JSON.stringify(this.props.campus) }
       </div>
       );
   }
 };
 
-export default Campus;
+const mapStateToProps = (state) => {
+  return {
+    campus: state
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCampus: (id) => dispatch(fetchCampusThunk(id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Campus);
